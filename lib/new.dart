b8315459bcd84971aa2a1d8faf7080844c283182
Ashok Durai <117632185+AshokWPD@users.noreply.github.com>
2023-09-12 14:09:44 +0530

    text

---
+++ lib/new.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class newww extends StatefulWidget {
+  const newww({super.key});
+
+  @override
+  State<newww> createState() => _newwwState();
+}
+
+class _newwwState extends State<newww> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
